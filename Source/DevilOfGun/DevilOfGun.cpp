c74b04e308f5ffe755359dd4ec36bbaecc9bec7b
Donghee <luz0415@naver.com>
2024-05-22 12:44:49 +0900

    Project Initialize

---
+++ Source/DevilOfGun/DevilOfGun.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "DevilOfGun.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DevilOfGun, "DevilOfGun" );
---
+++ Source/DevilOfGun/DevilOfGunGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DevilOfGunGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DEVILOFGUN_API ADevilOfGunGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
